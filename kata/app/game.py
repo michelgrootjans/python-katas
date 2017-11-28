15363e1d220948cf61131819cbafaaf9aeed7a1d
Michel Grootjans <michel.grootjans@gmail.com>
2017-11-28 13:24:58 +0100

    setup a simple test

---
+++ kata/app/game.py
@@ -0,0 +1,3 @@
+class Game(object):
+  def score(self):
+    return 0
--- kata/bowling-game-test.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-class BowlingGameTest(unittest.TestCase):
-  def test_one(self):
-    self.assertEqual(2, 2)
